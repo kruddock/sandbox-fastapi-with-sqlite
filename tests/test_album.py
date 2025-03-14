dab0457d1750003b8dd926407bf22127da2e6b04
kruddock <kevinruddock@gmail.com>
2025-03-14 19:19:22 +0000

    fix testing

--- tests/test_album.py
+++ tests/test_album.py
@@ -6,33 +6,8 @@ from sqlmodel import Session, SQLModel, create_engine, delete
 from dotenv import load_dotenv
 
 from main import app
-from repositories.repository_albums import AlbumRepository
-# from routes.album import get_session
-# from models import Album
-
-# @pytest.fixture(name="session")
-# def session_fixture():
-#     load_dotenv()
-
-#     TEST_DATABASE_URI: Final[str] = os.getenv('TEST_DATABASE_URI', '')
-    
-#     engine = create_engine(TEST_DATABASE_URI, connect_args={"check_same_thread": False})
-    
-#     SQLModel.metadata.create_all(engine)
-    
-#     with Session(engine) as session:
-#          yield session
-
-# @pytest.fixture(name="client")  
-# def client_fixture(session: Session):  
-#     def get_session_override():  
-#         return session
-
-#     app.dependency_overrides[get_session] = get_session_override  
-
-#     client = TestClient(app)  
-#     yield client  
-#     app.dependency_overrides.clear() 
+from repositories.repository_albums import AlbumRepository, get_repos
+from models import Album
 
 @pytest.fixture(name="engine")
 def engine_fixture():
@@ -47,18 +22,50 @@ def engine_fixture():
     yield engine
 
 @pytest.fixture(name="client")  
-def client_fixture(engine):  
+def client_fixture(engine):
     def get_test_repos():
         return AlbumRepository(engine)  
 
-    app.dependency_overrides['repos'] = get_test_repos
+    app.dependency_overrides[get_repos] = get_test_repos
 
     client = TestClient(app)  
     
     yield client
     
-    app.dependency_overrides.clear() 
+    app.dependency_overrides.clear()
+    
+    # load_dotenv()
 
+    # TEST_DATABASE_URI: Final[str] = os.getenv('TEST_DATABASE_URI', '')
+    
+    # engine = create_engine(TEST_DATABASE_URI, connect_args={"check_same_thread": False})
+    
+    # SQLModel.metadata.create_all(engine)
+    
+    # def get_test_repos():
+    #     return AlbumRepository(engine)  
+
+    # app.dependency_overrides['repos'] = get_test_repos
+
+    # client = TestClient(app)  
+    
+    # yield client
+    
+    # app.dependency_overrides.clear()
+
+@pytest.fixture(name="session")
+def session_fixture(engine):
+    with Session(engine) as session:
+        yield session
+
+@pytest.fixture(name="cleanup")
+def cleanup_fixture(engine):
+    yield
+    
+    with Session(engine) as session:
+        session.exec(delete(Album))
+        session.commit()
+        
 def test_album_not_found(client: TestClient):
     response = client.get("/albums/999")  
     
@@ -67,7 +74,7 @@ def test_album_not_found(client: TestClient):
     assert response.status_code == 404
     assert data["detail"] == "Entity not found"
 
-def test_create_album(client: TestClient):
+def test_create_an_album(client: TestClient):
     response = client.post(  
         "/albums/", json={ "title": "Blue Lights", "artist": "John Doe", "tracks": 10 }
     )
@@ -79,29 +86,26 @@ def test_create_album(client: TestClient):
     assert data["artist"] == "John Doe"  
     assert data["tracks"] == 10 
     assert data["id"] is not None 
+        
+def test_show_album(session: Session, client: TestClient):
+    album = Album(title="D-E-F", artist="Michelle", tracks=11)
+    session.add(album)
+    session.commit()
     
-# def test_show_album(session: Session, client: TestClient):
-#     album = Album(title="D-E-F", artist="Michelle", tracks=11)
-#     session.add(album)
-#     session.commit()
-
-#     response = client.get(f"/albums/{album.id}")
+    response = client.get(f"/albums/{album.id}")  
     
-#     data = response.json()
-
-#     assert response.status_code == 200
-#     assert data["title"] == album.title
-#     assert data["artist"] == album.artist
-#     assert data["tracks"] == album.tracks
-#     assert data["id"] == album.id
-
-# def test_list_of_albums_is_empty(session: Session, client: TestClient):
-    # response = client.get( "/albums")
-
-    # data = response.json()  
-
-    # assert response.status_code == 200  
-    # assert len(data) == 2 
+    data = response.json() 
     
-    # session.exec(delete(Album))
-    # session.commit()
+    assert response.status_code == 200
+    assert data["title"] == album.title
+    assert data["artist"] == album.artist
+    assert data["tracks"] == album.tracks
+    assert data["id"] == album.id
+
+def test_list_of_albums_is_empty(client: TestClient, cleanup: None):
+    response = client.get( "/albums")
+
+    data = response.json()  
+
+    assert response.status_code == 200  
+    assert len(data) == 2 
